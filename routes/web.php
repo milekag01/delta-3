4dce9a94fadd374edb217844f24814c48ff30374
milek agrawal <milekag01@gmail.com>
2020-06-26 00:42:40 +0530

    add API routes

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,41 @@ Route::group(array('before' => 'auth'), function()
 	Route::delete('clients/{id}', 'ClientsController@destroy');
     Route::resource('projects', 'ProjectsController', array('only' => array('show')));
 });
+
+//----------------- API routes
+Route::group(['prefix' => '/api/'], function()
+{	
+	// USER 
+    Route::get('user', 'UsersController@getUser');
+    Route::post('user/{id}', 'UsersController@updateUser');
+	Route::delete('user/', 'UsersController@deleteUser');
+
+	// CLIENT
+	Route::get('clients/{withWeight?}', 'ClientsController@getAllUserClients');
+	Route::put('clients/{id}', 'ClientsController@updateClient');
+	Route::post('clients', 'ClientsController@storeClient');
+	Route::delete('clients/{id}', 'ClientsController@removeClient');
+
+	// PROJECT
+    Route::get('projects/', 'ProjectsController@getAllUserProjects');
+    Route::get('projects/shared', 'ProjectsController@getAllMemberProjects');
+    Route::get('projects/{id}','ProjectsController@getProject');
+    Route::get('projects/{id}/owner','ProjectsController@getOwner');
+    Route::get('projects/{id}/members','ProjectsController@getMembers');
+	Route::post('projects', 'ProjectsController@storeProject');
+    Route::put('projects/{id}', 'ProjectsController@updateProject');
+    Route::post('projects/{id}/{email}/invite', 'ProjectsController@invite');
+    Route::delete('projects/{id}/{member_id}/remove', 'ProjectsController@removeMember' );
+
+	// TASK
+    Route::get('tasks', 'TasksController@getAllUserOpenTasks');
+    Route::post('tasks/{client_id}/{project_id}', 'TasksController@storeTask');
+    Route::delete('tasks/{id}', 'TasksController@removeTask');
+    Route::put('tasks/{id}', 'TasksController@updateTask');
+
+	// CREDENTIALS
+    Route::get('credentials/{id}','CredentialsController@getProjectCredentials');
+    Route::post('credentials', 'CredentialsController@storeCredential');
+    Route::put('credentials/{id}', 'CredentialsController@updateCredential');
+    Route::delete('credentials/{id}', 'CredentialsController@removeCredential');
+});
