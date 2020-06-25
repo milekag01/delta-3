c3bc37d8eacd162cbbe6f6fae824b1f6b05d9f68
milek agrawal <milekag01@gmail.com>
2020-06-26 01:02:34 +0530

    add database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,31 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UserSeeder::class);
-    }
+class DatabaseSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Eloquent::unguard();
+
+		$this->call('UsersTableSeeder');
+		$this->command->info('Users table seeded!');
+
+		$this->call('ClientsTableSeeder');
+		$this->command->info('Clients table seeded!');
+
+		$this->call('ProjectsTableSeeder');
+		$this->command->info('Projects table seeded!');
+
+		$this->call('TasksTableSeeder');
+		$this->command->info('Tasks table seeded!');
+
+		$this->command->info('Test Account: EMAIL: test@ribbbon.com PASSWORD: secret');
+
+	}
+
 }
