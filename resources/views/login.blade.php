a024edcd31a02e635c2b0efdbd0d6018006e3939
milek agrawal <milekag01@gmail.com>
2020-06-26 05:56:14 +0530

    add view login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,33 @@
+@extends('templates.outs.auth')
+
+@section('content')
+
+    <div class="special-form">
+        <a href="{{ route('home') }}"><img src="{{ \App\Helpers\Helpers::logoUrl()  }}" alt=""></a>
+        <h3 class="text-center">LOGIN</h3>
+
+        @if ($errors->first())
+            <span class="status-msg error-msg">{{ $errors->first() }}</span>
+        @endif
+
+        <hr>
+        {!! Form::open(array('action' => 'UsersController@login')) !!}
+        
+        <div class="form-group">
+            <label for="email" class="color-primary">Email:</label>
+            {!! Form::text( 'email', null, array('class' => 'form-control', "placeholder" => "Email","autofocus" => "true" )) !!}
+        </div>
+        
+        <div class="form-group">
+            <label for="password" class="color-primary">Password:</label>
+            {!! Form::password( 'password', array('class' => 'form-control', "placeholder" => "Password" )) !!}
+        </div>
+        
+        <div class="form-group">
+            {!! Form::submit( 'Login', array('class' => 'btn btn-primary btn-wide')) !!}
+        </div>
+        
+        {!! Form::close() !!}
+        <p>Don't have an account? <a href="{{ route('register') }}">register</a></p>
+    </div>
+@stop
