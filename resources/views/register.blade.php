ac615addfb789c5773e464bca59812aaa8fe34a1
milek agrawal <milekag01@gmail.com>
2020-06-26 09:40:14 +0530

    debug register form

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -11,28 +11,34 @@
         @endif
         
         <hr>
-        {!! Form::open(array('action' => 'UsersController@register')) !!}
+        <!-- {!! Form::open(array('action' => 'UsersController@register')) !!} -->
+        <form action="/make" method="post">
+            <div class="form-group">
+                <label for="fullName" class="color-primary">Full Name:</label>
+                <input type="text" class="form-control" name="fullName" placeholder="Full Name" autofocus required>
+                <!-- {!! Form::text('fullName', null, array('class' => 'form-control', "placeholder" => "Full name", "autofocus" => "true" )) !!} -->
+            </div>
+            
+            <div class="form-group">
+                <label for="email" class="color-primary">Email:</label>
+                <input type="email" class="form-control" name="email" placeholder="Email" required>
+                <!-- {!! Form::text('email', null, array('class' => 'form-control', "placeholder" => "Email" )) !!} -->
+            </div>
+            
+            <div class="form-group">
+                <label for="password" class="color-primary">Password:</label>
+                <input type="password" class="form-control" name="password" placeholder="Password" required>
+                <!-- {!! Form::password('password', array('class' => 'form-control', "placeholder" => "Password" )) !!} -->
+            </div>
+            
+            <div class="form-group">
+                <input type="submit" value="Register" class="btn btn-primary btn-wide">
+                <!-- {!! Form::submit('Register', array('class' => 'btn btn-primary btn-wide' )) !!} -->
+            </div>
+        </form>
         
-        <div class="form-group">
-            <label for="fullName" class="color-primary">Full Name:</label>
-            {!! Form::text('fullName', null, array('class' => 'form-control', "placeholder" => "Full name", "autofocus" => "true" )) !!}
-        </div>
         
-        <div class="form-group">
-            <label for="email" class="color-primary">Email:</label>
-            {!! Form::text('email', null, array('class' => 'form-control', "placeholder" => "Email" )) !!}
-        </div>
-        
-        <div class="form-group">
-            <label for="password" class="color-primary">Password:</label>
-            {!! Form::password('password', array('class' => 'form-control', "placeholder" => "Password" )) !!}
-        </div>
-        
-        <div class="form-group">
-            {!! Form::submit('Register', array('class' => 'btn btn-primary btn-wide' )) !!}
-        </div>
-        
-        {!! Form::close() !!}
+        <!-- {!! Form::close() !!} -->
         
         <p>Have an account? <a href="{{ route('login') }}">login</a></p>
     </div>
